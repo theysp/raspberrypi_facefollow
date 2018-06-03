c4d1e0993fb0983d5f6358379a871c0308797060
theysp <theysp@qq.com>
2018-06-03 23:56:24 +0800

    establish image transfer pipe

---
+++ imgclient.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import random
+import socket, select
+from time import gmtime, strftime
+from random import randint
+
+image = "1.png"
+
+HOST = '127.0.0.1'
+PORT = 6666
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = (HOST, PORT)
+sock.connect(server_address)
+print("connected to server")
+
+try:
+
+    # open image
+    myfile = open(image, 'rb')
+    bytes = myfile.read()
+    size = len(bytes)
+
+    # send image size to server
+    sock.sendall("SIZE %s" % size)
+    print("sent size to server")
+    answer = sock.recv(4096)
+
+    print 'answer = %s' % answer
+
+    # send image to server
+    if answer == 'GOT SIZE':
+        sock.sendall(bytes)
+
+        # check what server send
+        answer = sock.recv(4096)
+        print 'answer = %s' % answer
+
+        if answer == 'GOT IMAGE' :
+            sock.sendall("BYE BYE ")
+            print 'Image successfully send to server'
+
+    myfile.close()
+
+finally:
+    sock.close()
